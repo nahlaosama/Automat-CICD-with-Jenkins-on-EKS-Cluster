eb57da47bc5b176da7d867ca04370594dda7f6a6
nahlaosama <nahlaosama57@gmail.com>
2024-11-04 00:07:20 +0200

    files update

--- terraform-infra/modules/ec2/jenkins_role.tf
+++ terraform-infra/modules/ec2/jenkins_role.tf
@@ -13,7 +13,40 @@ resource "aws_iam_role" "jenkins_role" {
   })
 }
 
+resource "aws_iam_policy" "custom_deployment_policy" {
+  name        = "custom_deployment_policy"
+  description = "Policy to allow deployments and EKS operations"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:DescribeNodegroup",
+          "eks:DescribeFargateProfile"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+
 # Attach policies to the role
+
+resource "aws_iam_role_policy_attachment" "jenkins_eks_policy" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "custom_policy_attachment" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.custom_deployment_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "jenkins_ec2_policy" {
   role       = aws_iam_role.jenkins_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
